dcb5464958e8ba332feb3134bc27f1727e07ccdd
Bryce Kerley <bkerley@brycekerley.net>
2014-02-06 11:55:10 -0500

    initial

---
+++ lib/r509/cert/validator.rb
@@ -0,0 +1,9 @@
+require "r509/cert/validator/version"
+
+module R509
+  module Cert
+    module Validator
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/r509/cert/validator/version.rb
@@ -0,0 +1,7 @@
+module R509
+  module Cert
+    module Validator
+      VERSION = "0.0.1"
+    end
+  end
+end
